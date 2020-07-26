0e5ae845dc97d347ffde112adaec88d44d06e1cb
Katarina <katarinarosiak@gmail.com>
2020-07-26 10:33:27 +0200

    js101 folder with excercises and lessons

---
+++ js101/lessons/ex_easy1.js
@@ -0,0 +1,9 @@
+let isOdd = intArg => ((Math.abs(intArg)) % 2) ? true : false;
+
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
