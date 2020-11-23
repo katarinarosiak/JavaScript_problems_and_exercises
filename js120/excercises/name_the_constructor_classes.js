b689879c15a73cb88041a8f1b7cb2be99231f3fc
katarina <katarinarosiak@gmail.com>
2020-11-23 19:30:19 +0100

    small problems

---
+++ js120/excercises/name_the_constructor_classes.js
@@ -0,0 +1,11 @@
+// Update the following code so that, instead of logging the values, each statement logs the name of the constructor to which it belongs.
+
+
+console.log('Hello'.constructor.name);
+console.log([1, 2, 3].constructor.name);
+console.log({ name: 'Srdjan' }.constructor.name);
+
+
+// String
+// Array
+// Object
