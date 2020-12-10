2b57405ae9ef3d709673932bb0d7211e10e17df6
katarina <katarinarosiak@gmail.com>
2020-12-10 08:37:52 +0100

    excercises OOP

---
+++ js120/excercises/buggy_code1.js
@@ -0,0 +1,31 @@
+//Buggy Code 1
+
+function createGreeter(name) {
+  return {
+    name: name,
+    morning: 'Good Morning',
+    afternoon: 'Good Afternoon',
+    evening: 'Good Evening',
+    greet: function (timeOfDay) {
+      let msg = '';
+      switch (timeOfDay) {
+        case 'morning':
+          msg += `${this.morning} ${name}`;
+          break;
+        case 'afternoon':
+          msg += `${this.afternoon} ${name}`;
+          break;
+        case 'evening':
+          msg += `${this.evening} ${name}`;
+          break;
+      }
+      console.log(msg);
+    },
+  };
+}
+
+
+let helloVictor = createGreeter('Victor');
+helloVictor.greet('morning');
+
+// = Good Morning Victor
