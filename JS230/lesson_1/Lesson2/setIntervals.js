5d0bb192c7df8079ddaa9087ab9087c5d087e4f9
katarina <katarinarosiak@gmail.com>
2021-11-17 11:09:57 +0100

    exercises

--- JS230/lesson_1/Lesson2/setIntervals.js
+++ JS230/lesson_1/Lesson2/setIntervals.js
@@ -1,1 +1,44 @@
-setIntervals.js
+// 1. Write a function named startCounting that logs a number to the console every second, starting with 1. Each number should be one greater than the previous number.
+
+// function count() {
+//   let counter = 0; 
+//   return function () {
+//     counter++;
+//     console.log(counter); 
+//   }
+// }
+
+// function startCounting() {
+//   let func = count();
+//   setInterval(func, 1000)
+// }
+
+// startCounting(); 
+
+
+//2. Extend the code from the previous problem with a stopCounting function that stops the logger when called.
+
+let keepGoing = true;
+
+function stopCountting() {
+  keepGoing = false;
+};
+
+function count() {
+  let counter = 0;
+  return function () {
+    counter++;
+    if (keepGoing) {
+      console.log(counter);
+    }
+  }
+}
+
+function startCounting() {
+  let func = count();
+  setInterval(func, 1000)
+}
+
+startCounting();
+
+stopCountting();
--- JS230/lesson_1/dom_shuffling_completed/mainFixer.js
+++ JS230/lesson_1/dom_shuffling_completed/mainFixer.js
@@ -1,1 +1,23 @@
-mainFixer.js
+
+let h1 = document.querySelector('h1')
+let head = document.querySelector('head');
+head.appendChild(h1);
+
+let main = document.querySelector('main');
+let headers = document.querySelectorAll('header');
+let headerChildren = headers[0].children;
+let nav = document.querySelector('nav');
+headers[1].insertBefore(headerChildren[0], nav);
+headers[1].insertBefore(headerChildren[0], nav);
+
+document.body.insertBefore(headers[1], main);
+
+let figures = document.querySelectorAll('figure');
+let article = document.querySelector('article');
+article.appendChild(figures[1]);
+article.appendChild(figures[0]);
+
+nav.style.background = '#c0f0c0'
+
+
+
