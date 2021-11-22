91365c3ad4566a1e7da06a3a8ba5e04940cf3c98
katarina <katarinarosiak@gmail.com>
2021-11-22 16:32:35 +0100

    exercises

--- JS230/assignmenets/inputBox/inputBox.js
+++ JS230/assignmenets/inputBox/inputBox.js
@@ -1,14 +1,40 @@
-// Write JavaScript to add the focused class to the text-field element when the user clicks the element.
+// When the user presses the Backspace key while the text-field element has the focused class, delete the last character from the text within the content element.
+let intervalId;
+let isFocusedField;
 
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', () => {
   let textField = document.querySelector('.text-field');
 
   textField.addEventListener('click', event => {
+    event.stopPropagation();
     textField.classList.add('focused');
+    intervalId = intervalId || setInterval(function () {
+      textField.classList.toggle('cursor');
+    }, 500);
+
+    isFocusedField = true;
   });
-  document.addEventListener('click', event => {
-    textField.classList.remove('focused');
-  })
-
-
 });
+
+let stringVal = '';
+let content = document.querySelector('.content');
+document.addEventListener('keydown', e => {
+  if (isFocusedField) {
+    if (e.key === 'Backspace') {
+      stringVal = stringVal.slice(0, stringVal.length - 1);
+
+    } else {
+      stringVal += e.key;
+    }
+    content.textContent = stringVal;
+  };
+});
+
+document.addEventListener('click', event => {
+  clearInterval(intervalId);
+  let textField = document.querySelector('.text-field');
+  textField.classList.remove('focused');
+  textField.classList.remove('cursor');
+  isFocusedField = false;
+});
+
