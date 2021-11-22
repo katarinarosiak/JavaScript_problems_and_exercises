fdcd0a237021f9f1fbc23dc965d400f347b85bbe
katarina <katarinarosiak@gmail.com>
2021-11-22 09:34:34 +0100

    exercises

--- JS230/assignmenets/inputBox/inputBox.js
+++ JS230/assignmenets/inputBox/inputBox.js
@@ -1,1 +1,14 @@
-inputBox.js
+// Write JavaScript to add the focused class to the text-field element when the user clicks the element.
+
+document.addEventListener('DOMContentLoaded', event => {
+  let textField = document.querySelector('.text-field');
+
+  textField.addEventListener('click', event => {
+    textField.classList.add('focused');
+  });
+  document.addEventListener('click', event => {
+    textField.classList.remove('focused');
+  })
+
+
+});
