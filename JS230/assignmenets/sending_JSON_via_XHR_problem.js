6c480c9351da00733d05c01925d78d8f8abb90ea
katarina <katarinarosiak@gmail.com>
2021-12-16 15:55:38 +0100

    exercises

---
+++ JS230/assignmenets/sending_JSON_via_XHR_problem.js
@@ -0,0 +1,14 @@
+// Write out the raw text of the HTTP request the last example above will send to the server.
+
+POST
+Headers
+'Content-Type' = 'application/json; charset=utf-8'
+
+
+POST / books HTTP / 1.1
+Host: ls - 230 - book - catalog.herokuapp.com
+Headers:
+Content - Type : application / json; charset: utf - 8
+Accept: */*
+
+{"title": "Eloquent JavaScript", "author": "Marijn Haverbeke"}
---
+++ JS230/assignmenets/sending_JSON_via_XHR_problem2.js
@@ -0,0 +1,9 @@
+// Write some JavaScript to create a new product by sending a request to the JSON API on our web store. To create a product, make a POST request to https://ls-230-web-store-demo.herokuapp.com/v1/products. To make the post request, you'll need the following:
+
+// Content-Type header set to application/json; charset=utf-8
+// Authorization header set to token AUTH_TOKEN
+// json object with with the following properties:
+// name
+// sku (must have 3 or more characters)
+// price (must be an integer greater than 0)
+
